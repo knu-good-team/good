3fb3d495e1f13e2a63bcdf980b7a4879ea7fc9d0
SHIN BEOMSOO <beomsooshin.leo@gmail.com>
2024-05-29 14:55:56 +0900

    feat: 범죄구간지도 추가 & 로고 레이아웃 설정

---
+++ front/src/components/logo/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css'
+
+
+const Logo = () => {
+    return (
+        <>
+            <div className="logo">
+            </div>
+        </>
+    )
+}
+
+export default Logo;
