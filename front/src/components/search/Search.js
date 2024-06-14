585c4fb4c2b1c6325fbca1c54e370eeedd5a5f0d
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-06-14 19:34:56 +0900

    feat: front search box update

--- front/src/components/search/Search.js
+++ front/src/components/search/Search.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const Search = () => {
+    const [query, setQuery] = useState('');
+
+    const handleSearch = () => {
+        console.log('searching...')
+        // onSearch(query);
+    };
+
     return (
-        <div className="search">
-            Search Block
+        <div>
+            <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search..."
+            />
+            <button onClick={handleSearch}>Search</button>
         </div>
-    )
+    );
 }
 
 export default Search;
