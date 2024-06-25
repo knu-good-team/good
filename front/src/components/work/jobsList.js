bc5376d24504f3e1436cfb9a77d2c4268a6bfd9f
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-06-25 20:35:35 +0900

    feat: modal창 구현

--- front/src/components/work/jobsList.js
+++ front/src/components/work/jobsList.js
@@ -1,6 +1,18 @@
+import React, { useState } from 'react';
+import './joblist.css';
 import { PropTypes } from 'prop-types';
 
 const JobList = ({ jobs = [] }) => {
+    const [selectedJob, setSelectedJob] = useState(null)
+
+    const handleRowClick = (job) => {
+        setSelectedJob(job)
+    }
+
+    const closeModal = () => {
+        setSelectedJob(null)
+    }
+
     if (!jobs || jobs.length === 0) {
         return <p>No jobs available.</p>;
     }
@@ -21,7 +33,7 @@ const JobList = ({ jobs = [] }) => {
                 </thead>
                 <tbody>
                     {jobs.map(job => (
-                        <tr key={job.연번}>
+                        <tr key={job.연번} onClick={() => handleRowClick(job)} className="job-row">
                             <td>{job.사업장명}</td>
                             <td>{job.모집직종}</td>
                             <td>{job.고용형태}</td>
@@ -33,7 +45,74 @@ const JobList = ({ jobs = [] }) => {
                     ))}
                 </tbody>
             </table>
-        </div>
+            {selectedJob && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <h2 style={{marginLeft: "20px"}}>{selectedJob.사업장명}</h2>
+                        <div className="modal-grid">
+                            <div className="modal-column">
+                                <h3>기업정보</h3>
+                                <div className="modal-row">
+                                    <span>사업장명</span>
+                                    <span>{selectedJob.사업장명}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>연락처</span>
+                                    <span>{selectedJob.연락처}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>기업형태</span>
+                                    <span>{selectedJob.기업형태}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>담당기관</span>
+                                    <span>{selectedJob.담당기관}</span>
+                                </div>
+                            </div>
+                            <div className="modal-column">
+                                <h3>지원정보/자격</h3>
+                                <div className="modal-row">
+                                    <span>요구경력</span>
+                                    <span>{selectedJob.요구경력}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>요구학력</span>
+                                    <span>{selectedJob.요구학력}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>전공계열</span>
+                                    <span>{selectedJob.전공계열}</span>
+                                </div>
+                            </div>
+                            <div className="modal-column">
+                                <h3>근무조건</h3>
+                                <div className="modal-row">
+                                    <span>고용형태</span>
+                                    <span>{selectedJob.고용형태}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>임금/급여형태</span>
+                                    <span>{selectedJob.임금}/{selectedJob.임금형태}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>입사형태</span>
+                                    <span>{selectedJob.입사형태}</span>
+                                </div>
+                                <div className="modal-row">
+                                    <span>사업장주소</span>
+                                    <span>{selectedJob.사업장주소}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recruitment">
+                            <p>모집기간 {selectedJob.모집기간}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+            }
+        </div >
     );
 };
 
