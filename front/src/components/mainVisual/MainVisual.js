4f7462c8b4d1c3873963060fddc33b409050938c
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-06-25 21:00:06 +0900

    feat: search function 구현

--- front/src/components/mainVisual/MainVisual.js
+++ front/src/components/mainVisual/MainVisual.js
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import Work from '../work/work';
 import Banner from '../banner/banner';
 import Search from '../search/Search';
 
 const MainVisual = () => {
+    const [data, setData] = useState([]);
+    const [dataCount, setDataCount] = useState(0);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_DEV_URL}/disability/jobs`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(result => {
+                if (!result || result.data.length === 0) {
+                    throw new Error('No data available');
+                }
+                setData(result.data);
+                setDataCount(result.total);
+            })
+            .catch(error => {
+                setError(error.message);
+            });
+    }, []);
+
+    const fetchData = async (query) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(`${process.env.REACT_APP_DEV_URL}/disability/search?search=${query}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            setData(result.data);
+            setDataCount(result.total);
+        } catch (error) {
+            setError(error.message);
+        }
+        setLoading(false);
+    };
+
     return (
         <div className="main-container">
             <Banner />
             <div className="search-container">
                 <span className="search-text">취업 좋은 곳은 <span className="search-strength">좋은데<span className="check-mark"></span></span>에서!</span>
-                <Search />
+                <Search onSearch={fetchData} />
             </div>
-            <Work />
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                <div>
+                    <Work data={data} dataCount={dataCount} />
+                </div>
+            )}
         </div>
     )
 }
--- front/src/components/work/work.js
+++ front/src/components/work/work.js
@@ -1,58 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import './index.css';
-import Search from '../search/Search';
 import JobList from './jobsList';
 import Pagination from './pagination';
+import './index.css';
 
 
-const Work = () => {
-    const [data, setData] = useState([]);
-    const [dataCount, setDataCount] = useState(0);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+const Work = ({ data = [], dataCount = 0 }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(10);
 
     const indexOfLastItem = currentPage * itemsPerPage
     const indexOfFirstItem = indexOfLastItem - itemsPerPage
     const currentData = data.slice(indexOfFirstItem, indexOfLastItem)
-
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_DEV_URL}/disability/jobs`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(result => {
-                if (!result || result.data.length === 0) {
-                    throw new Error('No data available');
-                }
-                setData(result.data);
-                setDataCount(result.total);
-            })
-            .catch(error => {
-                setError(error.message);
-            });
-    }, []);
-
-    const fetchData = async (query) => {
-        setLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(`${process.env.REACT_APP_DEV_URL}/disability/search?search=${query}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const result = await response.json();
-            setData(result.data);
-            setDataCount(result.total);
-        } catch (error) {
-            setError(error.message);
-        }
-        setLoading(false);
-    };
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
@@ -63,21 +21,15 @@ const Work = () => {
                 {dataCount > 0 ? <div className="work-count">{dataCount}개의 데이터가 검색되었습니다.</div> : null}
                 {/* <Search onSearch={fetchData} /> */}
             </div>
-            {loading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>{error}</p>
-            ) : (
-                <div>
-                    <JobList jobs={currentData} />
-                    <Pagination
-                        itemsPerPage={itemsPerPage}
-                        totalItems={dataCount}
-                        paginate={paginate}
-                        currentPage={currentPage}
-                    />
-                </div>
-            )}
+            <div>
+                <JobList jobs={currentData} />
+                <Pagination
+                    itemsPerPage={itemsPerPage}
+                    totalItems={dataCount}
+                    paginate={paginate}
+                    currentPage={currentPage}
+                />
+            </div>
         </div>
     );
 };
