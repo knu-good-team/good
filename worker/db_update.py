3e3240ad29fde14cf172a4a0693441062db82a8e
Yang Jaehyeon <yjh3702@naver.com>
2024-06-28 22:46:04 +0900

    커밋 파일 수정
    modifyCheck
    
    appp.py upload

--- worker/db_update.py
+++ worker/db_update.py
@@ -7,13 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # PATH = "./data/disability_jobs_20231231.csv"
-PATH = "./data/disability_worker_20231231.csv"
+PATH = "./data/public_work_data.csv"
 USER = os.getenv('DB_USER')
 HOST = os.getenv('DB_HOST')
 PORT = os.getenv('DB_PORT')
+PASSWORD = os.getenv('DB_PW')
 DATABASE = os.getenv('DB_DATABASE')
 
 df = pd.read_csv(PATH, encoding='utf-8')
 # print(f'mysql+pymysql:///{USER}:@{HOST}:{PORT}/{DATABASE}')
-engine = create_engine(f'mysql+pymysql://{USER}:@{HOST}:{PORT}/{DATABASE}')
+engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
 df.to_sql('disability_workers', con=engine, if_exists='fail', index=False)
