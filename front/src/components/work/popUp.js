f0d3117d2d06e43eb891d067a501ee6050fc857c
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-07-05 12:07:25 +0900

    feat: kakao map에 사업장 pin 만들기

--- front/src/components/work/popUp.js
+++ front/src/components/work/popUp.js
@@ -36,7 +36,20 @@ const PopUp = ({ selectedJob, closeModal }) => {
                 center: new kakao.maps.LatLng(coordinate[1], coordinate[0]),
                 level: 3
             };
-            new kakao.maps.Map(container, options);
+            const map = new kakao.maps.Map(container, options);
+            //마커가 표시 될 위치
+            let markerPosition = new kakao.maps.LatLng(
+                coordinate[1],
+                coordinate[0]
+            );
+
+            // 마커를 생성
+            let marker = new kakao.maps.Marker({
+                position: markerPosition,
+            });
+
+            // 마커를 지도 위에 표시
+            marker.setMap(map);
         }
     }, [activeTab, coordinate])
 
