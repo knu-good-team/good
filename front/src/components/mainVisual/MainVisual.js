2302ddd567f662c1757dc6f94c0f2775d3383a1c
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-07-15 11:16:30 +0900

    PID

--- front/src/components/mainVisual/MainVisual.js
+++ front/src/components/mainVisual/MainVisual.js
@@ -47,10 +47,10 @@ const MainVisual = () => {
 
   return (
     <div className="main-container">
-      <Banner />
+      {/* <Banner /> */}
       <div className="search-container">
         <span className="search-text">
-          취업 좋은 곳은 <span className="search-strength">좋은데<span className="check-mark"></span></span>에서!
+          취업 좋은 곳은 <span className="search-strength">조은데<span className="check-mark"></span></span>에서!
         </span>
         <Search setSearchText={setSearchText} handleSearch={handleSearch} searchText={searchText} />
       </div>
